0f333da5c2b8782cbdfbbafa5f09c7ae33eb6fbe
ludaub <ludovic.aubriere@gmail.com>
2023-04-27 22:32:22 +0200

    build: add pwa

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,12 @@ import {
   LOCALE_ID,
   enableProdMode,
   importProvidersFrom,
+  isDevMode,
 } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
+import { provideServiceWorker } from '@angular/service-worker';
 
 import { Observable, tap } from 'rxjs';
 
@@ -53,5 +55,9 @@ bootstrapApplication(AppComponent, {
     provideAnimations(),
     provideHttpClient(withInterceptorsFromDi()),
     provideRouter(routes),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 }).catch((err) => console.error(err));
